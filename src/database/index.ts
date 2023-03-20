7dc824a6fd25512d14f0ad9d6e65998506b0874b
Felipi Matias <felipematiasoliveira16@gmail.com>
2023-03-20 18:18:20 -0300

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,3 @@
-import { Connection, createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-export default async (host = "database"): Promise<Connection> => {
-    const defaultOptions = await getConnectionOptions();
-
-    return createConnection(
-        Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost": host,
-            database:
-                process.env.NODE_ENV === "test" 
-                ? "fin_api" 
-                : defaultOptions.database,
-        })
-    );
-};
+(async () => await createConnection())();
